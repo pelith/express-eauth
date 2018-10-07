d2189e6ad9f9e53fdcae70563677c7438562d5b2
sirius <sirius@ugc25.csie.ncu.edu.tw>
2018-10-08 02:15:25 +0800

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+ * node-eth-auth
+ * Copyright(c) 2018 Pelith
+ * MIT Licensed
+ */
+
+'use strict';
+
+module.exports = require('./lib/eth-auth');
---
+++ test/test.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const EthAuth = require('../');
+
+const ethAuth = new EthAuth({"banner": "Pelieth"});
+
+class REQ {
+    constructor() {
+        this.params = {}
+    }
+}
+
+test('createMessage', function() {
+    let req = new REQ()
+    req.params = {
+        'Address':'0x550890336f0b5afd85fc355351372253e2491a6e'
+    }
+
+    let res = {}
+    let next = () => {}
+    ethAuth(req,res,next)
+
+    const regex = /\b[A-Fa-f0-9]{64}\b/
+    // assert.equal(regex.test(req.ethAuth.message[1].value), true);
+});
+
+test('confirmMessage', function() {
+    let req = new REQ()
+    req.params = {
+        'Message':'728b5d358098a338b6aca13e9f27369caac4acbd058c777c10ff97de83f62026',
+        'Signature': '0xe7edb643682ca6dbe96a6bb4b2b7cb31fd6ac2ad5ae10282641020823a4f088863e12b13354f16837046f6f8e0a21c6c2287e6ac575c18198a8d149da0d6d9c41b'
+    }
+
+    let res = {}
+    let next = () => {}
+    ethAuth(req,res,next)
+});
